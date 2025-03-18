c63fbe441e617e9debe51064bf0cea6cbfafc02b
Vlad Harkusha <sivari888@gmail.com>
2025-03-18 17:31:43 +0200

    errors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import { config } from "./configs/config";
 import { apiRouter } from "./routers/api.router";
+import { ApiError } from "./errors/api.error";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", apiRouter);
+
+app.use(
+    "*",
+    (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+        const status = err.status || 500;
+        const message = err.message ?? "Something went wrong";
+        res.status(status).json({ status, message });
+    },
+);
+
+process.on("uncaughtException", (err) => {
+    console.log("uncaughtException", err);
+    process.exit(1);
+});
 
 const dbConnection = async () => {
     let dbCon = false;
