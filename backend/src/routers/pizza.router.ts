201998db45c78612c37dc59dbd218d564eb88ce0
Vlad Harkusha <sivari888@gmail.com>
2025-04-03 16:20:12 +0300

    backend pizzas completed

--- backend/src/routers/pizza.router.ts
+++ backend/src/routers/pizza.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { pizzaController } from "../controllers/pizza.controller";
+import { commonMiddleware } from "../middlewares/common.middleware";
+import { PizzaValidator } from "../validators/pizza.validator";
 
 const router = Router();
 
 router.get("/", pizzaController.getAll);
-router.post("/", pizzaController.create);
+router.post("/", commonMiddleware.validateBody(PizzaValidator.create), pizzaController.create);
 
 export const pizzaRouter = router;
---
+++ backend/src/validators/pizza.validator.ts
@@ -0,0 +1,13 @@
+import joi from "joi";
+
+export class PizzaValidator {
+    private static name = joi.string().min(2).max(255).trim();
+    private static price = joi.number().min(1).max(1_000_000);
+    private static diameter = joi.number().min(1).max(255);
+
+    public static create = joi.object({
+        name: this.name.required(),
+        price: this.price.required(),
+        diameter: this.diameter.required(),
+    });
+}
